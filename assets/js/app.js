d3bca8b51f629e080df21cb3ca7fcffd4563e7fa
alfecaesar <alfecaesar@gmail.com>
2024-05-31 21:46:30 +0800

    major updates on layout and add subpages

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,54 @@
 // An object literal
 var app = {
   init: function() {
-    app.functionOne();
+    console.log('🟢 Cedar of Hope ⛪');
+
+    app.menuActive();
   },
-  functionOne: function () {
+  menuActive: function() {
+    var headerMenu = document.querySelector('ul.navbar-nav');
+    var footerMenu = document.querySelector('ul.footer-nav');
+
+    var pagePath = document.location.pathname;
+
+    headerMenu.querySelectorAll('li').forEach(function(item){
+      if(item.querySelector('a').getAttribute('href') === pagePath){
+        item.classList.add('selected');
+      }
+    });
+
+    footerMenu.querySelectorAll('li').forEach(function(item){
+      if(item.querySelector('a').getAttribute('href') === pagePath){
+        item.classList.add('selected');
+      }
+    });
+
   },
-  scrollTop: function() {
-    window.scrollTo({top: 0, behavior: 'smooth'});
+  gCalendar: function() {
+    document.addEventListener('DOMContentLoaded', function() {
+      var calendarEl = document.getElementById('gcalendar');
+    
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            timeZone: '############',
+            googleCalendarApiKey: '#################', // googleCalendarApiKey
+            headerToolbar: {
+                start: 'prev,next today',
+                center: 'title',
+                end: 'dayGridMonth,timeGridWeek,timeGridDay,listYear'
+            },
+            events: '######@gmail.com' // calendarId
+        });
+    
+        calendar.render();
+
+        console.log(calendar.googleCalendarApiKey);
+    });
   }
 };
 (function() {
-  // your page initialization code here
-  // the DOM will be available here
   app.init();
+
+  if(document.getElementById('gcalendar')){
+    app.gCalendar();
+  }
 })();
